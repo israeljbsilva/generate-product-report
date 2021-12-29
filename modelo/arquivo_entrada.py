ddf513830779a83df1de1686ad73cf75764253bc
Israel Bernardes <israel.bernardes.silva@gmail.com>
2021-12-29 18:17:19 -0300

    parte 2

--- modelo/arquivo_entrada.py
+++ modelo/arquivo_entrada.py
@@ -4,6 +4,27 @@ class ArquivoEntrada:
         self.cabecalho = None
         self.produtos = []
 
+    def ler_arquivo_de_entrada(self):
+        arquivo_entrada = open("arquivo-entrada-produtos.csv", "r")
+        linhas_arquivo = arquivo_entrada.readlines()
+        arquivo_entrada.close()
+        return linhas_arquivo
+
+    def construir_arquivo_entrada(self):
+        linhas_arquivo = self.ler_arquivo_de_entrada()
+        for linha in linhas_arquivo:
+            linhas_separadas = linha.split(';')
+
+            if linhas_separadas[0] == 'H':
+                objeto_cabecalho = Cabecalho()
+                objeto_cabecalho.construir_cabecalho(linhas_separadas)
+                self.cabecalho = objeto_cabecalho
+
+            elif linhas_separadas[0] == 'P':
+                objeto_produto = Produto()
+                objeto_produto.construir_produto(linhas_separadas)
+                self.produtos.append(objeto_produto)
+
 
 class Cabecalho:
 
@@ -11,6 +32,10 @@ class Cabecalho:
         self.identificador = 'H'
         self.titulo = None
         self.data = None
+
+    def construir_cabecalho(self, linhas_separadas):
+        self.titulo = linhas_separadas[1]
+        self.data = linhas_separadas[2]
 
 
 class Produto:
@@ -27,3 +52,15 @@ class Produto:
         self.preco = None
         self.data_lancamento = None
         self.url_imagem = None
+
+    def construir_produto(self, linhas_separadas):
+        self.nome = linhas_separadas[1]
+        self.marca = linhas_separadas[2]
+        self.tamanho_tela = linhas_separadas[3]
+        self.cor = linhas_separadas[4]
+        self.processador = linhas_separadas[5]
+        self.memoria_ram = linhas_separadas[6]
+        self.espaco_interno = linhas_separadas[7]
+        self.preco = linhas_separadas[8]
+        self.data_lancamento = linhas_separadas[9]
+        self.url_imagem = linhas_separadas[10]
--- principal.py
+++ principal.py
@@ -1,35 +1,10 @@
-from modelo.arquivo_entrada import Cabecalho, Produto, ArquivoEntrada
-
-arquivo_entrada = open("arquivo-entrada-produtos.csv", "r")
-linhas_arquivo = arquivo_entrada.readlines()
+from modelo.arquivo_entrada import ArquivoEntrada
 
 
+# lendo o arquivo de entrada e colocando em objeto
 objeto_arquivo_entrada = ArquivoEntrada()
+objeto_arquivo_entrada.construir_arquivo_entrada()
 
-for linha in linhas_arquivo:
-    linhas_separadas = linha.split(';')
+# traducao desse arquivo de entrada para o template (.html)
 
-    if linhas_separadas[0] == 'H':
-        objeto_cabecalho = Cabecalho()
-        objeto_cabecalho.titulo = linhas_separadas[1]
-        objeto_cabecalho.data = linhas_separadas[2]
-        objeto_arquivo_entrada.cabecalho = objeto_cabecalho
-
-    elif linhas_separadas[0] == 'P':
-        objeto_produto = Produto()
-        objeto_produto.nome = linhas_separadas[1]
-        objeto_produto.marca = linhas_separadas[2]
-        objeto_produto.tamanho_tela = linhas_separadas[3]
-        objeto_produto.cor = linhas_separadas[4]
-        objeto_produto.processador = linhas_separadas[5]
-        objeto_produto.memoria_ram = linhas_separadas[6]
-        objeto_produto.espaco_interno = linhas_separadas[7]
-        objeto_produto.preco = linhas_separadas[8]
-        objeto_produto.data_lancamento = linhas_separadas[9]
-        objeto_produto.url_imagem = linhas_separadas[10]
-
-        objeto_arquivo_entrada.produtos.append(objeto_produto)
-
-arquivo_entrada.close()
-
-print(objeto_arquivo_entrada)
+# escrever um arquivo de saida
