a9ee985b29faf9d08e4a32925998de293db067d5
Israel Bernardes <israel.bernardes.silva@gmail.com>
2022-01-02 15:37:50 -0300

    incluir main

--- principal.py
+++ principal.py
@@ -2,12 +2,13 @@ from modelo.arquivo_entrada_csv import ArquivoEntrada
 from tradutor.relatorio_html import renderizar_template, escrever_relatorio_html
 
 
-# le e constroi arquivo de entrada
-objeto_arquivo_entrada = ArquivoEntrada()
-objeto_arquivo_entrada.construir_arquivo_entrada()
+if __name__ == '__main__':
+    # le e constroi arquivo de entrada
+    objeto_arquivo_entrada = ArquivoEntrada()
+    objeto_arquivo_entrada.construir_arquivo_entrada()
 
-# le o template e constroi o relatorio
-template_texto_pronto = renderizar_template(objeto_arquivo_entrada)
+    # le o template e constroi o relatorio
+    template_texto_pronto = renderizar_template(objeto_arquivo_entrada)
 
-# escrever arquivo de saida
-escrever_relatorio_html(template_texto_pronto)
+    # escrever arquivo de saida
+    escrever_relatorio_html(template_texto_pronto)
