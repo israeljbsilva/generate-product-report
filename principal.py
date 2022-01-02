cdc3557d1281e9943ca9ffba13f4b34e35bbf6c5
Israel Bernardes <israel.bernardes.silva@gmail.com>
2022-01-02 15:38:29 -0300

    refatorar

--- principal.py
+++ principal.py
@@ -3,12 +3,9 @@ from tradutor.relatorio_html import renderizar_template, escrever_relatorio_html
 
 
 if __name__ == '__main__':
-    # le e constroi arquivo de entrada
     objeto_arquivo_entrada = ArquivoEntrada()
     objeto_arquivo_entrada.construir_arquivo_entrada()
 
-    # le o template e constroi o relatorio
     template_texto_pronto = renderizar_template(objeto_arquivo_entrada)
 
-    # escrever arquivo de saida
     escrever_relatorio_html(template_texto_pronto)
